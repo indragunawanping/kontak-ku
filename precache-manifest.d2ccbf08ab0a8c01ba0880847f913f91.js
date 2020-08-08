f17a7d40e5350524798c3af4daec4878cdcf56cd
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-08-08 23:43:27 +0700

    Updates

---
+++ precache-manifest.d2ccbf08ab0a8c01ba0880847f913f91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f6d0dfc975c1ccc348822695676d74",
+    "url": "/kontak-ku/index.html"
+  },
+  {
+    "revision": "36c4f2c375333e5427fb",
+    "url": "/kontak-ku/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e04c7dd6d898921c72a2",
+    "url": "/kontak-ku/static/js/2.51fa8f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kontak-ku/static/js/2.51fa8f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c4f2c375333e5427fb",
+    "url": "/kontak-ku/static/js/main.69e614e6.chunk.js"
+  },
+  {
+    "revision": "786abe9450b74d731bb6",
+    "url": "/kontak-ku/static/js/runtime-main.f65ae1b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kontak-ku/static/media/logo.5d5d9eef.svg"
+  }
+]);
