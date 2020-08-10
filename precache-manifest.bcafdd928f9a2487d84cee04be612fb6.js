2e0a266ccf80847fd9295cfa0a26893fe94281bc
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-08-10 08:55:59 +0700

    Updates

--- precache-manifest.bcafdd928f9a2487d84cee04be612fb6.js
+++ precache-manifest.bcafdd928f9a2487d84cee04be612fb6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "557c9b959f50454065cac320ff21dcfc",
+    "revision": "5b84370b34cb34b37f75d330896dac6a",
     "url": "/kontak-ku/index.html"
   },
   {
-    "revision": "c6b2e7e1fd53167c006b",
-    "url": "/kontak-ku/static/css/main.5859bf82.chunk.css"
+    "revision": "7a39a2b919199fb1d2c5",
+    "url": "/kontak-ku/static/css/main.9795ee2c.chunk.css"
   },
   {
-    "revision": "f8f1f91fd57f481dd3bf",
-    "url": "/kontak-ku/static/js/2.c53a6904.chunk.js"
+    "revision": "511ff8eda7ee379988f8",
+    "url": "/kontak-ku/static/js/2.c07cec15.chunk.js"
   },
   {
     "revision": "5bd09eb7f2cf4d145d532d862be18942",
-    "url": "/kontak-ku/static/js/2.c53a6904.chunk.js.LICENSE.txt"
+    "url": "/kontak-ku/static/js/2.c07cec15.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c6b2e7e1fd53167c006b",
-    "url": "/kontak-ku/static/js/main.474d21dc.chunk.js"
+    "revision": "7a39a2b919199fb1d2c5",
+    "url": "/kontak-ku/static/js/main.46e51d63.chunk.js"
   },
   {
     "revision": "786abe9450b74d731bb6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kontak-ku/precache-manifest.13c732d42d9d70ea1a47007357a73058.js"
+  "/kontak-ku/precache-manifest.bcafdd928f9a2487d84cee04be612fb6.js"
 );
 
 self.addEventListener('message', (event) => {
